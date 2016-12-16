ad2ad2cbbe3abf6565242c9c21c84738f26b3faa
Sathit Seethaphon <dixonsatit@gmail.com>
2016-12-16 11:03:09 +0700

    use AuthChoice Widget

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,5 +1,5 @@
 <?php
-
+use yii\authclient\widgets\AuthChoice;
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
@@ -11,43 +11,18 @@ $this->title = 'My Yii Application';
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
+        <?php $authAuthChoice = AuthChoice::begin([
+            'baseAuthUrl' => ['/user/security/auth'],
+            //'baseAuthUrl' => ['/site/auth'],
+            'popupMode' => true,
+            ]); ?>
+                <?php foreach ($authAuthChoice->getClients() as $key => $client): ?>
+                    <?= $authAuthChoice->clientLink($client,'DOH Login',['class'=>'btn btn-success btn-lg']) ?>
+                <?php endforeach; ?>
+            <?php AuthChoice::end(); ?>
 
     </div>
+
+
 </div>
