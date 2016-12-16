a81c6055515962700faa351b70140c7033d48e2a
Sathit Seethaphon <dixonsatit@gmail.com>
2016-12-16 11:01:02 +0700

    config log to db

--- console/config/main.php
+++ console/config/main.php
@@ -23,7 +23,7 @@ return [
         'log' => [
             'targets' => [
                 [
-                    'class' => 'yii\log\FileTarget',
+                    'class' => 'yii\log\DbTarget',
                     'levels' => ['error', 'warning'],
                 ],
             ],
